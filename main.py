f196d56cf5084c6fa13706f8c48a8d46d040a4a2
serv <liuyuhua69@gmail.com>
2025-08-01 14:44:38 +0800

    readme update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dump import MyDump, MyImport, Mysql
 
 if __name__ == "__main__":
     config = ConfigParser()
-    config.read('config-p3.ini')
+    config.read('config.ini')
     source = Mysql(config.get('source', 'db_host'), config.get('source', 'db_port'), config.get('source', 'db_user'),
                    config.get('source', 'db_pass'))
     target = Mysql(config.get('target', 'db_host'), config.get('target', 'db_port'), config.get('target', 'db_user'),
